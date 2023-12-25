2d520735f6d244ebc8813f22d9d52d6b0fbd8e5e
comicalboxer <154823477+comicalboxer@users.noreply.github.com>
2023-12-25 21:15:11 +0530

    Add files via upload

---
+++ OnlineVotingSystem/voters/inc/footer.php
@@ -0,0 +1,15 @@
+
+    <div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+            <p> &copy; PranviKant Mishra - All Rights   <br />
+                Class:11a
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
